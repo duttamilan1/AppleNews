8c50259ce68888227fe6c209aea8d5b8d3c07e89
Milan Dutta <duttamilan191@gmail.com>
2022-11-12 13:53:50 -0500

    Initial Commit

---
+++ Apple News/Apple_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_NewsApp.swift
+//  Apple News
+//
+//  Created by Milan Dutta on 11/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Apple_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
